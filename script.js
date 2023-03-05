077cce7d9e68f12cf0cf6d0fc5bac41bcea15fff
flanderos <andershellerud@hotmail.no>
2023-03-05 20:03:12 +0100

    Working calcilator

---
+++ script.js
@@ -0,0 +1,13 @@
+const output = document.querySelector(".output");
+
+document.querySelectorAll("#btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "C") {
+      output.textContent = "";
+    } else if (button.textContent === "=") {
+      output.textContent = eval(output.textContent);
+    } else {
+      output.textContent += button.textContent;
+    }
+  });
+});
